86df7a9f2d89ba886eb4ccf6b0d571be38e7bc80
Michael <68875800+michaelabebaws@users.noreply.github.com>
2025-03-16 17:46:59 +0300

    customized features and the webpage layout

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 let container = document.getElementById("container");
 
 // Loop to create 16 div elements
-for (let i = 0; i < 16; i++) {
-    let grid = document.createElement("div"); // Create a new div
-    container.appendChild(grid); // Append to the container
+for (let i = 0; i < 256; i++) {
+    let grids = document.createElement("div"); // Create a new div
+    container.appendChild(grids); // Append to the container
+    grids.classList.add("grid-square");
+
+    grids.addEventListener("mouseover", function() {
+        this.classList.add("hovered"); // Add class (method 1)
+        // this.style.backgroundColor = "darkblue"; // Alternative (method 2)
+    });
 }
