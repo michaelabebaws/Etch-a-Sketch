81d68deb896e0f39887a77f51bc569e174658943
Michael <68875800+michaelabebaws@users.noreply.github.com>
2025-03-16 22:20:25 +0300

    final version updated

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-// Select the container div
 let container = document.getElementById("container");
+let gridSize = 16;
 
-// Loop to create 16 div elements
-for (let i = 0; i < 256; i++) {
-    let grids = document.createElement("div"); // Create a new div
-    container.appendChild(grids); // Append to the container
-    grids.classList.add("grid-square");
+function createGrid (size) {
 
-    grids.addEventListener("mouseover", function() {
-        this.classList.add("hovered"); // Add class (method 1)
-        // this.style.backgroundColor = "darkblue"; // Alternative (method 2)
-    });
+    container.innerHTML = "";
+    let squareSize = 960 / size;
+
+    for (let i = 0; i < size * size; i++) {
+
+        let grids = document.createElement("div"); 
+        grids.classList.add("grid-square");
+        grids.style.width = `${squareSize}px`;
+        grids.style.height = `${squareSize}px`;
+        
+        container.appendChild(grids); 
+        grids.addEventListener("mouseover", function() {
+        this.classList.add("hovered"); });
+    }
 }
+
+function generateGrid() {
+    let newSize = prompt("Enter new grid size (Max: 100):");
+    newSize = parseInt(newSize);
+
+    if (Number.isInteger(newSize) && newSize >= 1 && newSize <= 100) {
+        gridSize = newSize;
+        createGrid(gridSize);
+    }else {
+        alert("Invalid input! Please enter a number between 1 and 100.");
+    }
+}
+
+function resetGrid() {
+    let squares = document.querySelectorAll(".grid-square");
+    squares.forEach(square => square.classList.remove("hovered"));
+}
+
+
+createGrid(gridSize)
